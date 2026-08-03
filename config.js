8c8947e125ca9198a0cfbf1dd6b14aabef0a3bc7
Quiz User <trt33813381@gmail.com>
2026-08-03 10:48:00 +0900

    config.js를 GitHub에 추가

---
+++ config.js
@@ -0,0 +1,9 @@
+// Firebase 설정 (이 파일은 .gitignore에 있어서 GitHub에 올라가지 않습니다)
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAThdHk8H3N_nenNVkNqtVQ-djoOfLE7bk",
+  authDomain: "cow-quiz.firebaseapp.com",
+  projectId: "cow-quiz",
+  storageBucket: "cow-quiz.firebasestorage.app",
+  messagingSenderId: "93309366969",
+  appId: "1:93309366969:web:09e1f5cae053209d18a168"
+};
